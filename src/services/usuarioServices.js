2808fc5d7cd6dbdc6aca09fd6d7dd19838c5822d
MariaDerkacheva <derkachevamaria@gmail.com>
2024-11-03 14:22:06 -0300

    autenticacion de usuarios

--- src/services/usuarioServices.js
+++ src/services/usuarioServices.js
@@ -10,6 +10,19 @@ export default class UsuariosService {
         return this.usuarios.buscarPorId(id);
     }
 
+    buscar = async (correoElectronico, contrasenia) => {
+
+        console.log("Correo y contraseña recibidos:", correoElectronico, contrasenia);
+
+        const usuario = await this.usuarios.buscar(correoElectronico, contrasenia);
+
+        console.log("Usuario encontrado:", usuario); // Debe mostrar el usuario si lo encuentra
+
+        return usuario;
+    }
+
+
+
     buscarTodos= ()=>{
         return this.usuarios.buscarTodos();
     }
