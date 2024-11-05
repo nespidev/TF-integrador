c38fcd47eebc273f5f494317f84dbdd931df09ff
NicoOjeda <nico_ojeda86@hotmail.com>
2024-11-05 12:53:55 -0300

    se agrega validacion de usuario admin en crud  usuarios tipo

--- src/v1/routes/usuariosTipoRoutes.js
+++ src/v1/routes/usuariosTipoRoutes.js
@@ -1,16 +1,18 @@
 import express from "express";
 import UsuariosTipoController from "../../controllers/usuariosTipoController.js";
+import { auth } from "../../middlewares/authMiddleware.js";
+import { Roles } from "../../utiles/roles.js"
 
 const router = express.Router();
 
 const usuariosTipoController = new UsuariosTipoController();
 
-router.get("/", usuariosTipoController.buscarTodos);
+router.get("/",auth([Roles.ADMIN]), usuariosTipoController.buscarTodos);
 
-router.get("/:id", usuariosTipoController.buscarPorId); 
+router.get("/:id",auth([Roles.ADMIN]), usuariosTipoController.buscarPorId); 
 
-router.post('/', usuariosTipoController.crear);
+router.post('/',auth([Roles.ADMIN]), usuariosTipoController.crear);
 
-router.patch("/:id", usuariosTipoController.actualizar)
+router.patch("/:id",auth([Roles.ADMIN]), usuariosTipoController.actualizar)
 
 export {router};
